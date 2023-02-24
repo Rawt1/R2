ab84fa0c9a79ab4f5e60f0a6fdb4ba2259282e4e
rawt <rawt@softwaremind.com>
2023-02-24 12:58:25 +0100

    installation scripts

---
+++ kube/infra/infra-install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+istioctl install -f "${SCRIPTPATH}/istio-operator.yaml" &&
+kubectl wait --for=condition=ready pod -l app=istiod -n istio-system &&
+echo "Istio is ready" &&
+kubectl apply -f "${SCRIPTPATH}/namespaces" &&
+echo "Namespaces are ready"
